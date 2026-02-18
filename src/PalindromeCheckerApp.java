cec98064a0388d05249dcbe2fcdf74701dce2fb7
Code-syntaxvs <vaibhavshrivastava333@gmail.com>
2026-02-18 15:16:48 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,29 @@
-public class PalindromeCheckerApp {
+/**
+ * UseCase1PalindromeCheckerApp
+ * UC1: Application Entry & Welcome Message
+ *
+ * Objective:
+ * Display welcome message and application details at startup.
+ */
+
+public class UseCase1PalindromeCheckerApp {
+
+    // Main method - Entry point of Java application
+    public static void main(String[] args) {
+
+        // Display Application Name
+        System.out.println("=====================================");
+        System.out.println("     Welcome to Palindrome Checker App");
+        System.out.println("=====================================");
+
+        // Display Application Version
+        System.out.println("Application Version : 1.0.0");
+
+        // Display Short Description
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("-------------------------------------");
+
+        // End of UC1
+        System.out.println("Application Started Successfully!");
+    }
 }
